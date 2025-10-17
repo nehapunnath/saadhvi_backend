cf1b15bbe448fb1ec0e0f27b580ee0330cfba5a5
neha <nehapunnath@gmail.com>
2025-10-17 17:53:02 +0530

    'two'

--- Config/firebaseAdmin.js
+++ Config/firebaseAdmin.js
@@ -1,11 +1,15 @@
 const admin = require('firebase-admin');
-const serviceAccount = require('./serviceAccount.json');
+// const serviceAccount = require('./serviceAccount.json');
+    const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT);
+
 
 if (!admin.apps.length) {
   admin.initializeApp({
     credential: admin.credential.cert(serviceAccount),
-    databaseURL: `https://saadhvi-silks-default-rtdb.firebaseio.com`,
-    storageBucket: `saadhvi-silks.firebasestorage.app` 
+    // databaseURL: `https://saadhvi-silks-default-rtdb.firebaseio.com`,
+    // storageBucket: `saadhvi-silks.firebasestorage.app` 
+    databaseURL: process.env.FIREBASE_DATABASE ,
+    storageBucket: process.env.FIREBASE_STORAGE
   });
 }
 
